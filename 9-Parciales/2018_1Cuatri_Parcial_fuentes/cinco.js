5aeecd0f3fa0653041bc6ce8f8fe97f3ad5bba8c
NicoPraco <47321381+NicoPraco@users.noreply.github.com>
2019-07-12 22:05:39 -0300

    Trabajo realizado en la clase 12/07
    Algunos ejercicios de el parcial realizados, practicar el resto.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,34 @@
-function mostrar()
-{
+function mostrar() {
+    var planeta;
+    var mensaje;
+
+    planeta = prompt("Ingrese un planeta");
+
+    switch(planeta){
+            case "mercurio":
+            case "venus":
+                mensaje = "Acá hace mas calor";
+                break;
+
+            case "tierra":
+                mensaje = "Acá vivimos";
+                break;
+
+            case "marte":
+            case "jupiter":
+            case "saturno":
+            case "urano":
+            case "neptuno":
+            case "plutón":
+                mensaje = "Acá hace mas frio";
+                break;
+
+            default:
+                mensaje = "El nombre ingresado no corresponde a un planeta"
+                break;
+
+    }
+
+    alert(mensaje);
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,30 @@
-function mostrar()
-{
+function mostrar() {
+    var num1;
+    var num2;
+    var resultado;
+
+    num1 = prompt("Ingrese un número");
+    num2 = prompt("Ingrese un número");
+
+    if (num1 == num2) {
+        resultado = num1 + num2;
+
+    } else if (num1 > num2) {
+
+        resultado = num1 - num2;
+
+    } else {
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
+
+        resultado = num1 + num2;
+
+        if ((num1 + num2) > 10) {
+           resultado = "La suma es de " + resultado + " y supero el 10";
+        }
+
+    }
+
+    alert(resultado);
 
 }
