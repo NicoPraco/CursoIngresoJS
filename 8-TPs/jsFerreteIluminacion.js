7a6d4be5ff8601462d100a70f9014b2223efe19c
NicoPraco <47321381+NicoPraco@users.noreply.github.com>
2019-07-12 22:03:44 -0300

    Trabajo realizado en la clase 12/07
    Algunos ejercicios del TPs hechos, faltan realizar. (HACER FERRETE ILUMINACIÓN)

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -8,7 +8,23 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	
+function CalcularPrecio() {
+    //Declaracion de variables
+    var cantidad;
+    var marca;
+    var precio;
+    var descuento;
+
+    //Entrada
+    cantidad = document.getElementById("Cantidad").value;
+    marca = document.getElementById("Marca").value;
+
+    if (cantidad >= 6) {
+        
+
+    }
+
+
+
+
 }
