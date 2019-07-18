2815bf0935fc7485994f96d47de1617fa3f69a0d
NicoPraco <47321381+NicoPraco@users.noreply.github.com>
2019-07-17 22:10:25 -0300

    Clase 17/07
    Se apago la PC. Tengo que hacer los 3 primeros ejercicios del TP y  algún otro de parcial si no me equivoco.

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,76 +1,77 @@
 /*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
-A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%.
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
- ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó.
 
  */
 function CalcularPrecio() {
-    //Declaracion de variables
-    var cantidad;
-    var marca;
-    var precio = 35;
-    var descuento;
-    var precioFinal;
-    var IIBB;
-    var mensaje;
+  //Declaracion de variables
+  var cantidad;
+  var marca;
+  var precio = 35;
+  var precioConcantidad;
+  var descuento;
+  var precioFinal;
+  var IIBB = 10;
+  var importe;
+  var mensaje;
 
-    //Entrada
-    cantidad = document.getElementById("Cantidad").value;
-    marca = document.getElementById("Marca").value;
+  //Entrada
+  cantidad = parseInt(document.getElementById("Cantidad").value);
+  marca = document.getElementById("Marca").value;
 
-    //Procesamiento
-    if (cantidad >= 6) {
-        descuento = 50;
+  //Procesamiento
+  if (cantidad >= 6) {
+    descuento = 50;
+
+  } else if (cantidad == 5) {
+    if (marca == "ArgentinaLuz") {
+      descuento = 40;
 
     } else {
-        if (cantidad == 5) {
-            if (marca == "ArgentinaLuz") {
-                descuento = 40;
-
-            } else {
-                descuento = 30
-
-            }
-
-        } else {
-            if (cantidad == 4) {
-                if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
-                    descuento = 25;
-
-                } else {
-                    descuento = 20;
-
-                }
-            } else {
-                if (cantidad == 3) {
-                    if (marca == "ArgentinaLuz") {
-                        descuento = 15;
-
-                    } else {
-                        if (marca == "FelipeLamparas") {
-                            descuento = 10;
-
-                        } else {
-                            descuento = 5;
-
-                        }
-                    }
-
-                }
-            }
-        }
+      descuento = 30;
 
     }
 
-    if (precioFinal >= 120) {
-        IIBB = 10
-        precioFinal = precioFinal + (precioFinal * IIBB / 100);
-        mensaje = "Usted pago " + 
+  } else if (cantidad == 4) {
+    if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+      descuento = 25;
+
+    } else {
+      descuento = 20;
 
     }
+  } else if (cantidad == 3) {
+    if (marca == "ArgentinaLuz") {
+      descuento = 15;
+
+    } else if (marca == "FelipeLamparas") {
+      descuento = 10;
+
+    } else {
+      descuento = 5;
+
+    }
+
+  }
+
+  precioConcantidad = precio * cantidad;
+
+  precioFinal = precioConcantidad - (precioConcantidad * descuento / 100);
+
+  //Salida
+  document.getElementById("precioDescuento").value = precioFinal;
+
+  if (precioFinal >= 120) {
+    importe = precioFinal * IIBB / 100;
+    precioFinal = precioFinal + importe;
+    mensaje = "Usted pago $" + importe + " de Ingresos Brutos."
+    alert(mensaje);
+
+  }
 
 }
